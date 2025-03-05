9318c06f4877ddfc5c8899d5fa08b5a5447d61fc
jackiboi307 <jack.josefsson@gmail.com>
2025-03-05 23:27:43 +0100

    changed from octal escapes to hex, which made the rust literals correct

--- for_importing/escape_sequences.go
+++ for_importing/escape_sequences.go
@@ -2,131 +2,131 @@ const (
 
     // Control codes
 
-    BELL                   string = "\007"
+    BELL                   string = "\x07"
 
     // Cursor control
 
-    CUR_HOME               string = "\033[H"
-    CUR_SET                string = "\033[%s;%sH"
-    CUR_UP                 string = "\033[%sA"
-    CUR_DOWN               string = "\033[%sB"
-    CUR_RIGHT              string = "\033[%sC"
-    CUR_LEFT               string = "\033[%sD"
-    CUR_DOWN_BEG           string = "\033[%sE"
-    CUR_UP_BEG             string = "\033[%sF"
-    CUR_COL                string = "\033[%sG"
+    CUR_HOME               string = "\x1b[H"
+    CUR_SET                string = "\x1b[%s;%sH"
+    CUR_UP                 string = "\x1b[%sA"
+    CUR_DOWN               string = "\x1b[%sB"
+    CUR_RIGHT              string = "\x1b[%sC"
+    CUR_LEFT               string = "\x1b[%sD"
+    CUR_DOWN_BEG           string = "\x1b[%sE"
+    CUR_UP_BEG             string = "\x1b[%sF"
+    CUR_COL                string = "\x1b[%sG"
 
-    CUR_UP_ONE             string = "\033[1A"
-    CUR_DOWN_ONE           string = "\033[1B"
-    CUR_RIGHT_ONE          string = "\033[1C"
-    CUR_LEFT_ONE           string = "\033[1D"
+    CUR_UP_ONE             string = "\x1b[1A"
+    CUR_DOWN_ONE           string = "\x1b[1B"
+    CUR_RIGHT_ONE          string = "\x1b[1C"
+    CUR_LEFT_ONE           string = "\x1b[1D"
 
-    CUR_COL_HOME           string = "\033[0G"
+    CUR_COL_HOME           string = "\x1b[0G"
 
-    CUR_HIDE               string = "\033[?25l"
-    CUR_SHOW               string = "\033[?25h"
+    CUR_HIDE               string = "\x1b[?25l"
+    CUR_SHOW               string = "\x1b[?25h"
 
-    CUR_SAVE_DEC           string = "\033 " // DEC
-    CUR_RESTORE_DEC        string = "\033 " // DEC 
-    CUR_SAVE_SCO           string = "\033[" // SCO
-    CUR_RESTORE_SCO        string = "\033[" // SCO
+    CUR_SAVE_DEC           string = "\x1b " // DEC
+    CUR_RESTORE_DEC        string = "\x1b " // DEC 
+    CUR_SAVE_SCO           string = "\x1b[" // SCO
+    CUR_RESTORE_SCO        string = "\x1b[" // SCO
 
     // Screen operations
 
-    SCREEN_SAVE            string = "\033[?47h"
-    RESTORE_SAVE           string = "\033[?47l"
+    SCREEN_SAVE            string = "\x1b[?47h"
+    RESTORE_SAVE           string = "\x1b[?47l"
 
-    ERASE_SCREEN           string = "\033[2J"
-    ERASE_LINE             string = "\033[2K"
-    ERASE_TO_END           string = "\033[0J"
-    ERASE_TO_LINE_END      string = "\033[0J"
+    ERASE_SCREEN           string = "\x1b[2J"
+    ERASE_LINE             string = "\x1b[2K"
+    ERASE_TO_END           string = "\x1b[0J"
+    ERASE_TO_LINE_END      string = "\x1b[0J"
 
     // Style
 
-    RESET                  string = "\033[0m"
-    BOLD                   string = "\033[1m"
-    DIM                    string = "\033[2m"
-    ITALIC                 string = "\033[3m"
-    UNDERLINE              string = "\033[4m"
-    BLINK                  string = "\033[5m"
-    STRIKE                 string = "\033[9m"
+    RESET                  string = "\x1b[0m"
+    BOLD                   string = "\x1b[1m"
+    DIM                    string = "\x1b[2m"
+    ITALIC                 string = "\x1b[3m"
+    UNDERLINE              string = "\x1b[4m"
+    BLINK                  string = "\x1b[5m"
+    STRIKE                 string = "\x1b[9m"
 
     // 256 colors
 
-    FG_ID                  string = "\033[38;5;%sm"
-    BG_ID                  string = "\033[48;5;%sm"
+    FG_ID                  string = "\x1b[38;5;%sm"
+    BG_ID                  string = "\x1b[48;5;%sm"
 
     // True color
 
-    FG_RGB                 string = "\033[38;2;%s;%s;%sm"
-    BG_RGB                 string = "\033[48;2;%s;%s;%sm"
+    FG_RGB                 string = "\x1b[38;2;%s;%s;%sm"
+    BG_RGB                 string = "\x1b[48;2;%s;%s;%sm"
 
     // 16 color codes
 
-    FG_BLACK               string = "\033[30m"
-    FG_RED                 string = "\033[31m"
-    FG_GREEN               string = "\033[32m"
-    FG_YELLOW              string = "\033[33m"
-    FG_BLUE                string = "\033[34m"
-    FG_MAGENTA             string = "\033[35m"
-    FG_CYAN                string = "\033[36m"
-    FG_WHITE               string = "\033[37m"
-    FG_DEFAULT             string = "\033[39m"
+    FG_BLACK               string = "\x1b[30m"
+    FG_RED                 string = "\x1b[31m"
+    FG_GREEN               string = "\x1b[32m"
+    FG_YELLOW              string = "\x1b[33m"
+    FG_BLUE                string = "\x1b[34m"
+    FG_MAGENTA             string = "\x1b[35m"
+    FG_CYAN                string = "\x1b[36m"
+    FG_WHITE               string = "\x1b[37m"
+    FG_DEFAULT             string = "\x1b[39m"
 
-    BG_BLACK               string = "\033[40m"
-    BG_RED                 string = "\033[41m"
-    BG_GREEN               string = "\033[42m"
-    BG_YELLOW              string = "\033[43m"
-    BG_BLUE                string = "\033[44m"
-    BG_MAGENTA             string = "\033[45m"
-    BG_CYAN                string = "\033[46m"
-    BG_WHITE               string = "\033[47m"
-    BG_DEFAULT             string = "\033[49m"
+    BG_BLACK               string = "\x1b[40m"
+    BG_RED                 string = "\x1b[41m"
+    BG_GREEN               string = "\x1b[42m"
+    BG_YELLOW              string = "\x1b[43m"
+    BG_BLUE                string = "\x1b[44m"
+    BG_MAGENTA             string = "\x1b[45m"
+    BG_CYAN                string = "\x1b[46m"
+    BG_WHITE               string = "\x1b[47m"
+    BG_DEFAULT             string = "\x1b[49m"
 
     // Bright versions
 
-    FG_BLACK_B             string = "\033[90m"
-    FG_RED_B               string = "\033[91m"
-    FG_GREEN_B             string = "\033[92m"
-    FG_YELLOW_B            string = "\033[93m"
-    FG_BLUE_B              string = "\033[94m"
-    FG_MAGENTA_B           string = "\033[95m"
-    FG_CYAN_B              string = "\033[96m"
-    FG_WHITE_B             string = "\033[97m"
+    FG_BLACK_B             string = "\x1b[90m"
+    FG_RED_B               string = "\x1b[91m"
+    FG_GREEN_B             string = "\x1b[92m"
+    FG_YELLOW_B            string = "\x1b[93m"
+    FG_BLUE_B              string = "\x1b[94m"
+    FG_MAGENTA_B           string = "\x1b[95m"
+    FG_CYAN_B              string = "\x1b[96m"
+    FG_WHITE_B             string = "\x1b[97m"
 
-    BG_BLACK_B             string = "\033[100m"
-    BG_RED_B               string = "\033[101m"
-    BG_GREEN_B             string = "\033[102m"
-    BG_YELLOW_B            string = "\033[103m"
-    BG_BLUE_B              string = "\033[104m"
-    BG_MAGENTA_B           string = "\033[105m"
-    BG_CYAN_B              string = "\033[106m"
-    BG_WHITE_B             string = "\033[107m"
+    BG_BLACK_B             string = "\x1b[100m"
+    BG_RED_B               string = "\x1b[101m"
+    BG_GREEN_B             string = "\x1b[102m"
+    BG_YELLOW_B            string = "\x1b[103m"
+    BG_BLUE_B              string = "\x1b[104m"
+    BG_MAGENTA_B           string = "\x1b[105m"
+    BG_CYAN_B              string = "\x1b[106m"
+    BG_WHITE_B             string = "\x1b[107m"
 
     // Keys
 
-    ESCAPE                 string = "\033"
-    RETURN                 string = "\r"
+    ESCAPE                 string = "\x1b"
+    RETURN                 string = "\x0d"
     BACKSPACE              string = "\x7f"
-    TAB                    string = "\t"
-    TAB_SHIFT              string = "\033[Z"
+    TAB                    string = "\x09"
+    TAB_SHIFT              string = "\x1b[Z"
 
     // Arrows
 
-    ARROW_UP               string = "\033[A"
-    ARROW_DOWN             string = "\033[B"
-    ARROW_LEFT             string = "\033[D"
-    ARROW_RIGHT            string = "\033[C"
-    ARROW_UP_SHIFT         string = "\033[1;2A"
-    ARROW_DOWN_SHIFT       string = "\033[1;2B"
-    ARROW_LEFT_SHIFT       string = "\033[1;2D"
-    ARROW_RIGHT_SHIFT      string = "\033[1;2C"
-    ARROW_UP_CTRL          string = "\033[1;5A"
-    ARROW_DOWN_CTRL        string = "\033[1;5B"
-    ARROW_LEFT_CTRL        string = "\033[1;5D"
-    ARROW_RIGHT_CTRL       string = "\033[1;5C"
-    ARROW_UP_CTRL_SHIFT    string = "\033[1;6A"
-    ARROW_DOWN_CTRL_SHIFT  string = "\033[1;6B"
-    ARROW_LEFT_CTRL_SHIFT  string = "\033[1;6D"
-    ARROW_RIGHT_CTRL_SHIFT string = "\033[1;6C"
+    ARROW_UP               string = "\x1b[A"
+    ARROW_DOWN             string = "\x1b[B"
+    ARROW_LEFT             string = "\x1b[D"
+    ARROW_RIGHT            string = "\x1b[C"
+    ARROW_UP_SHIFT         string = "\x1b[1;2A"
+    ARROW_DOWN_SHIFT       string = "\x1b[1;2B"
+    ARROW_LEFT_SHIFT       string = "\x1b[1;2D"
+    ARROW_RIGHT_SHIFT      string = "\x1b[1;2C"
+    ARROW_UP_CTRL          string = "\x1b[1;5A"
+    ARROW_DOWN_CTRL        string = "\x1b[1;5B"
+    ARROW_LEFT_CTRL        string = "\x1b[1;5D"
+    ARROW_RIGHT_CTRL       string = "\x1b[1;5C"
+    ARROW_UP_CTRL_SHIFT    string = "\x1b[1;6A"
+    ARROW_DOWN_CTRL_SHIFT  string = "\x1b[1;6B"
+    ARROW_LEFT_CTRL_SHIFT  string = "\x1b[1;6D"
+    ARROW_RIGHT_CTRL_SHIFT string = "\x1b[1;6C"
 )
--- for_importing/escape_sequences.rs
+++ for_importing/escape_sequences.rs
@@ -4,130 +4,130 @@ type StringType = &'static str;
 
 // Control codes
 
-pub const BELL:                   StringType = "\007";
+pub const BELL:                   StringType = "\x07";
 
 // Cursor control
 
-pub const CUR_HOME:               StringType = "\033[H";
-pub const CUR_SET:                StringType = "\033[{};{}H";
-pub const CUR_UP:                 StringType = "\033[{}A";
-pub const CUR_DOWN:               StringType = "\033[{}B";
-pub const CUR_RIGHT:              StringType = "\033[{}C";
-pub const CUR_LEFT:               StringType = "\033[{}D";
-pub const CUR_DOWN_BEG:           StringType = "\033[{}E";
-pub const CUR_UP_BEG:             StringType = "\033[{}F";
-pub const CUR_COL:                StringType = "\033[{}G";
+pub const CUR_HOME:               StringType = "\x1b[H";
+pub const CUR_SET:                StringType = "\x1b[{};{}H";
+pub const CUR_UP:                 StringType = "\x1b[{}A";
+pub const CUR_DOWN:               StringType = "\x1b[{}B";
+pub const CUR_RIGHT:              StringType = "\x1b[{}C";
+pub const CUR_LEFT:               StringType = "\x1b[{}D";
+pub const CUR_DOWN_BEG:           StringType = "\x1b[{}E";
+pub const CUR_UP_BEG:             StringType = "\x1b[{}F";
+pub const CUR_COL:                StringType = "\x1b[{}G";
 
-pub const CUR_UP_ONE:             StringType = "\033[1A";
-pub const CUR_DOWN_ONE:           StringType = "\033[1B";
-pub const CUR_RIGHT_ONE:          StringType = "\033[1C";
-pub const CUR_LEFT_ONE:           StringType = "\033[1D";
+pub const CUR_UP_ONE:             StringType = "\x1b[1A";
+pub const CUR_DOWN_ONE:           StringType = "\x1b[1B";
+pub const CUR_RIGHT_ONE:          StringType = "\x1b[1C";
+pub const CUR_LEFT_ONE:           StringType = "\x1b[1D";
 
-pub const CUR_COL_HOME:           StringType = "\033[0G";
+pub const CUR_COL_HOME:           StringType = "\x1b[0G";
 
-pub const CUR_HIDE:               StringType = "\033[?25l";
-pub const CUR_SHOW:               StringType = "\033[?25h";
+pub const CUR_HIDE:               StringType = "\x1b[?25l";
+pub const CUR_SHOW:               StringType = "\x1b[?25h";
 
-pub const CUR_SAVE_DEC:           StringType = "\033 "; // DEC
-pub const CUR_RESTORE_DEC:        StringType = "\033 "; // DEC 
-pub const CUR_SAVE_SCO:           StringType = "\033["; // SCO
-pub const CUR_RESTORE_SCO:        StringType = "\033["; // SCO
+pub const CUR_SAVE_DEC:           StringType = "\x1b "; // DEC
+pub const CUR_RESTORE_DEC:        StringType = "\x1b "; // DEC 
+pub const CUR_SAVE_SCO:           StringType = "\x1b["; // SCO
+pub const CUR_RESTORE_SCO:        StringType = "\x1b["; // SCO
 
 // Screen operations
 
-pub const SCREEN_SAVE:            StringType = "\033[?47h";
-pub const RESTORE_SAVE:           StringType = "\033[?47l";
+pub const SCREEN_SAVE:            StringType = "\x1b[?47h";
+pub const RESTORE_SAVE:           StringType = "\x1b[?47l";
 
-pub const ERASE_SCREEN:           StringType = "\033[2J";
-pub const ERASE_LINE:             StringType = "\033[2K";
-pub const ERASE_TO_END:           StringType = "\033[0J";
-pub const ERASE_TO_LINE_END:      StringType = "\033[0J";
+pub const ERASE_SCREEN:           StringType = "\x1b[2J";
+pub const ERASE_LINE:             StringType = "\x1b[2K";
+pub const ERASE_TO_END:           StringType = "\x1b[0J";
+pub const ERASE_TO_LINE_END:      StringType = "\x1b[0J";
 
 // Style
 
-pub const RESET:                  StringType = "\033[0m";
-pub const BOLD:                   StringType = "\033[1m";
-pub const DIM:                    StringType = "\033[2m";
-pub const ITALIC:                 StringType = "\033[3m";
-pub const UNDERLINE:              StringType = "\033[4m";
-pub const BLINK:                  StringType = "\033[5m";
-pub const STRIKE:                 StringType = "\033[9m";
+pub const RESET:                  StringType = "\x1b[0m";
+pub const BOLD:                   StringType = "\x1b[1m";
+pub const DIM:                    StringType = "\x1b[2m";
+pub const ITALIC:                 StringType = "\x1b[3m";
+pub const UNDERLINE:              StringType = "\x1b[4m";
+pub const BLINK:                  StringType = "\x1b[5m";
+pub const STRIKE:                 StringType = "\x1b[9m";
 
 // 256 colors
 
-pub const FG_ID:                  StringType = "\033[38;5;{}m";
-pub const BG_ID:                  StringType = "\033[48;5;{}m";
+pub const FG_ID:                  StringType = "\x1b[38;5;{}m";
+pub const BG_ID:                  StringType = "\x1b[48;5;{}m";
 
 // True color
 
-pub const FG_RGB:                 StringType = "\033[38;2;{};{};{}m";
-pub const BG_RGB:                 StringType = "\033[48;2;{};{};{}m";
+pub const FG_RGB:                 StringType = "\x1b[38;2;{};{};{}m";
+pub const BG_RGB:                 StringType = "\x1b[48;2;{};{};{}m";
 
 // 16 color codes
 
-pub const FG_BLACK:               StringType = "\033[30m";
-pub const FG_RED:                 StringType = "\033[31m";
-pub const FG_GREEN:               StringType = "\033[32m";
-pub const FG_YELLOW:              StringType = "\033[33m";
-pub const FG_BLUE:                StringType = "\033[34m";
-pub const FG_MAGENTA:             StringType = "\033[35m";
-pub const FG_CYAN:                StringType = "\033[36m";
-pub const FG_WHITE:               StringType = "\033[37m";
-pub const FG_DEFAULT:             StringType = "\033[39m";
+pub const FG_BLACK:               StringType = "\x1b[30m";
+pub const FG_RED:                 StringType = "\x1b[31m";
+pub const FG_GREEN:               StringType = "\x1b[32m";
+pub const FG_YELLOW:              StringType = "\x1b[33m";
+pub const FG_BLUE:                StringType = "\x1b[34m";
+pub const FG_MAGENTA:             StringType = "\x1b[35m";
+pub const FG_CYAN:                StringType = "\x1b[36m";
+pub const FG_WHITE:               StringType = "\x1b[37m";
+pub const FG_DEFAULT:             StringType = "\x1b[39m";
 
-pub const BG_BLACK:               StringType = "\033[40m";
-pub const BG_RED:                 StringType = "\033[41m";
-pub const BG_GREEN:               StringType = "\033[42m";
-pub const BG_YELLOW:              StringType = "\033[43m";
-pub const BG_BLUE:                StringType = "\033[44m";
-pub const BG_MAGENTA:             StringType = "\033[45m";
-pub const BG_CYAN:                StringType = "\033[46m";
-pub const BG_WHITE:               StringType = "\033[47m";
-pub const BG_DEFAULT:             StringType = "\033[49m";
+pub const BG_BLACK:               StringType = "\x1b[40m";
+pub const BG_RED:                 StringType = "\x1b[41m";
+pub const BG_GREEN:               StringType = "\x1b[42m";
+pub const BG_YELLOW:              StringType = "\x1b[43m";
+pub const BG_BLUE:                StringType = "\x1b[44m";
+pub const BG_MAGENTA:             StringType = "\x1b[45m";
+pub const BG_CYAN:                StringType = "\x1b[46m";
+pub const BG_WHITE:               StringType = "\x1b[47m";
+pub const BG_DEFAULT:             StringType = "\x1b[49m";
 
 // Bright versions
 
-pub const FG_BLACK_B:             StringType = "\033[90m";
-pub const FG_RED_B:               StringType = "\033[91m";
-pub const FG_GREEN_B:             StringType = "\033[92m";
-pub const FG_YELLOW_B:            StringType = "\033[93m";
-pub const FG_BLUE_B:              StringType = "\033[94m";
-pub const FG_MAGENTA_B:           StringType = "\033[95m";
-pub const FG_CYAN_B:              StringType = "\033[96m";
-pub const FG_WHITE_B:             StringType = "\033[97m";
+pub const FG_BLACK_B:             StringType = "\x1b[90m";
+pub const FG_RED_B:               StringType = "\x1b[91m";
+pub const FG_GREEN_B:             StringType = "\x1b[92m";
+pub const FG_YELLOW_B:            StringType = "\x1b[93m";
+pub const FG_BLUE_B:              StringType = "\x1b[94m";
+pub const FG_MAGENTA_B:           StringType = "\x1b[95m";
+pub const FG_CYAN_B:              StringType = "\x1b[96m";
+pub const FG_WHITE_B:             StringType = "\x1b[97m";
 
-pub const BG_BLACK_B:             StringType = "\033[100m";
-pub const BG_RED_B:               StringType = "\033[101m";
-pub const BG_GREEN_B:             StringType = "\033[102m";
-pub const BG_YELLOW_B:            StringType = "\033[103m";
-pub const BG_BLUE_B:              StringType = "\033[104m";
-pub const BG_MAGENTA_B:           StringType = "\033[105m";
-pub const BG_CYAN_B:              StringType = "\033[106m";
-pub const BG_WHITE_B:             StringType = "\033[107m";
+pub const BG_BLACK_B:             StringType = "\x1b[100m";
+pub const BG_RED_B:               StringType = "\x1b[101m";
+pub const BG_GREEN_B:             StringType = "\x1b[102m";
+pub const BG_YELLOW_B:            StringType = "\x1b[103m";
+pub const BG_BLUE_B:              StringType = "\x1b[104m";
+pub const BG_MAGENTA_B:           StringType = "\x1b[105m";
+pub const BG_CYAN_B:              StringType = "\x1b[106m";
+pub const BG_WHITE_B:             StringType = "\x1b[107m";
 
 // Keys
 
-pub const ESCAPE:                 StringType = "\033";
-pub const RETURN:                 StringType = "\r";
+pub const ESCAPE:                 StringType = "\x1b";
+pub const RETURN:                 StringType = "\x0d";
 pub const BACKSPACE:              StringType = "\x7f";
-pub const TAB:                    StringType = "\t";
-pub const TAB_SHIFT:              StringType = "\033[Z";
+pub const TAB:                    StringType = "\x09";
+pub const TAB_SHIFT:              StringType = "\x1b[Z";
 
 // Arrows
 
-pub const ARROW_UP:               StringType = "\033[A";
-pub const ARROW_DOWN:             StringType = "\033[B";
-pub const ARROW_LEFT:             StringType = "\033[D";
-pub const ARROW_RIGHT:            StringType = "\033[C";
-pub const ARROW_UP_SHIFT:         StringType = "\033[1;2A";
-pub const ARROW_DOWN_SHIFT:       StringType = "\033[1;2B";
-pub const ARROW_LEFT_SHIFT:       StringType = "\033[1;2D";
-pub const ARROW_RIGHT_SHIFT:      StringType = "\033[1;2C";
-pub const ARROW_UP_CTRL:          StringType = "\033[1;5A";
-pub const ARROW_DOWN_CTRL:        StringType = "\033[1;5B";
-pub const ARROW_LEFT_CTRL:        StringType = "\033[1;5D";
-pub const ARROW_RIGHT_CTRL:       StringType = "\033[1;5C";
-pub const ARROW_UP_CTRL_SHIFT:    StringType = "\033[1;6A";
-pub const ARROW_DOWN_CTRL_SHIFT:  StringType = "\033[1;6B";
-pub const ARROW_LEFT_CTRL_SHIFT:  StringType = "\033[1;6D";
-pub const ARROW_RIGHT_CTRL_SHIFT: StringType = "\033[1;6C";
+pub const ARROW_UP:               StringType = "\x1b[A";
+pub const ARROW_DOWN:             StringType = "\x1b[B";
+pub const ARROW_LEFT:             StringType = "\x1b[D";
+pub const ARROW_RIGHT:            StringType = "\x1b[C";
+pub const ARROW_UP_SHIFT:         StringType = "\x1b[1;2A";
+pub const ARROW_DOWN_SHIFT:       StringType = "\x1b[1;2B";
+pub const ARROW_LEFT_SHIFT:       StringType = "\x1b[1;2D";
+pub const ARROW_RIGHT_SHIFT:      StringType = "\x1b[1;2C";
+pub const ARROW_UP_CTRL:          StringType = "\x1b[1;5A";
+pub const ARROW_DOWN_CTRL:        StringType = "\x1b[1;5B";
+pub const ARROW_LEFT_CTRL:        StringType = "\x1b[1;5D";
+pub const ARROW_RIGHT_CTRL:       StringType = "\x1b[1;5C";
+pub const ARROW_UP_CTRL_SHIFT:    StringType = "\x1b[1;6A";
+pub const ARROW_DOWN_CTRL_SHIFT:  StringType = "\x1b[1;6B";
+pub const ARROW_LEFT_CTRL_SHIFT:  StringType = "\x1b[1;6D";
+pub const ARROW_RIGHT_CTRL_SHIFT: StringType = "\x1b[1;6C";
